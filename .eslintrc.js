dad96bf90bf8b204005fd8fbe24066dc19520d19
Hung-Han (Henry) Chen <1474479+chenhunghan@users.noreply.github.com>
2020-11-13 11:17:36 +0800

    Add .js for eslintrc
    Signed-off-by: Hung-Han (Henry) Chen <1474479+chenhunghan@users.noreply.github.com>

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  "env": {
+    "node": true,
+    "es2020": true
+  },
+  "parserOptions": {
+    "sourceType": "module"
+  },
+  "extends": "eslint:recommended",
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "double"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ]
+  }
+};
