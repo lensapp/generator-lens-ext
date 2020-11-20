d7b6595baaad16463c9b0ea7ae2231f582aafe8d
Hung-Han (Henry) Chen <1474479+chenhunghan@users.noreply.github.com>
2020-11-20 13:05:49 +0800

    Reference root package.json in template, so dont have to maintain two package versions
    Signed-off-by: Hung-Han (Henry) Chen <1474479+chenhunghan@users.noreply.github.com>

--- generators/app/deps.js
+++ generators/app/deps.js
@@ -1,12 +1,9 @@
+const { devDependencies, dependencies } = require("../../package.json");
+
 module.exports = {
-  "@k8slens/extensions": "^4.0.0-beta.3",
   "ts-loader": "^8.0.11",
-  "typescript": "^4.1.2",
-  "@types/react": "^16.9.56",
-  "@types/node": "^12.12.9",
   "webpack": "^4.44.2",
   "webpack-cli": "^3.3.11",
-  "eslint": "^7.13.0",
-  "@typescript-eslint/eslint-plugin": "^4.8.1",
-  "@typescript-eslint/parser": "^4.8.1",
+  ...devDependencies,
+  ...dependencies,
 };
