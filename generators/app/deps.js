b8823409401328416fda9c7e5f9365373c726115
Hung-Han (Henry) Chen <1474479+chenhunghan@users.noreply.github.com>
2020-11-26 20:01:58 +0800

    Add versions not in parent package.json

--- generators/app/deps.js
+++ generators/app/deps.js
@@ -1,9 +1,15 @@
 const { devDependencies, dependencies } = require("../../package.json");
 
 module.exports = {
+  "react-dom": "^17.0.1",
   "ts-loader": "^8.0.11",
   "webpack": "^4.44.2",
   "webpack-cli": "^3.3.11",
+  "@babel/preset-env": "^7.12.7",
+  "@babel/preset-react": "^7.12.7",
+  "@babel/preset-typescript": "^7.12.7",
+  "electron": "^11.0.3",
+  "@jest-runner/electron": "^3.0.0",
   ...devDependencies,
   ...dependencies,
 };
