232bc3d7d917dfbe3476d7ec503953fdfec253d0
Hung-Han (Henry) Chen <chenhungh@gmail.com>
2022-05-09 16:39:14 +0300

    Should be 4.9.2
    Signed-off-by: Hung-Han (Henry) Chen <chenhungh@gmail.com>

--- generators/app/deps.js
+++ generators/app/deps.js
@@ -3,7 +3,7 @@ const { devDependencies, dependencies } = require("../../package.json");
 module.exports = {
   "ts-loader": "^9.3.0",
   "webpack": "^5.72.0",
-  "webpack-cli": "^4.9.21",
+  "webpack-cli": "^4.9.2",
   "@babel/preset-env": "^7.17.10",
   "@babel/preset-react": "^7.16.7",
   "@babel/preset-typescript": "^7.16.7",
