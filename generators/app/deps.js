219f67fbe65a2a769b139e5b0607940ee24070c9
chh <1474479+chenhunghan@users.noreply.github.com>
2022-05-09 16:19:19 +0300

    Support Lens 5 (#35)

--- generators/app/deps.js
+++ generators/app/deps.js
@@ -1,14 +1,12 @@
 const { devDependencies, dependencies } = require("../../package.json");
 
 module.exports = {
-  "ts-loader": "^8.0.11",
-  "webpack": "^4.44.2",
-  "webpack-cli": "^3.3.11",
-  "@babel/preset-env": "^7.12.7",
-  "@babel/preset-react": "^7.12.7",
-  "@babel/preset-typescript": "^7.12.7",
-  "electron": "^11.0.3",
-  "@jest-runner/electron": "^3.0.0",
+  "ts-loader": "^9.3.0",
+  "webpack": "^5.72.0",
+  "webpack-cli": "^4.9.21",
+  "@babel/preset-env": "^7.17.10",
+  "@babel/preset-react": "^7.16.7",
+  "@babel/preset-typescript": "^7.16.7",
   ...devDependencies,
   ...dependencies,
 };
