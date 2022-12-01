ffb31a6a0250e1eb4263a59b91200677272314d1
Sebastian Malton <sebastian@malton.name>
2022-12-01 07:40:38 -0800

    Support Lens 6 (#37)
    * Support Lens 6
    
    Signed-off-by: Sebastian Malton <sebastian@malton.name>
    
    * Update workflows
    
    Signed-off-by: Sebastian Malton <sebastian@malton.name>
    
    * Update test
    
    Signed-off-by: Sebastian Malton <sebastian@malton.name>
    
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- generators/app/deps.js
+++ generators/app/deps.js
@@ -1,8 +1,8 @@
 const { devDependencies, dependencies } = require("../../package.json");
 
 module.exports = {
-  "ts-loader": "^9.3.0",
-  "webpack": "^5.72.0",
+  "ts-loader": "^9.4.1",
+  "webpack": "^5.75.0",
   "webpack-cli": "^4.9.2",
   "@babel/preset-env": "^7.17.10",
   "@babel/preset-react": "^7.16.7",
